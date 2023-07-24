7be00507546762e7160b0b46082fa5adb1faab9e
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2023-07-24 15:43:15 +0530

    adding url in reset passwrord file

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
 const bcrypt = require("bcrypt");
 const crypto=require("crypto");
+require("dotenv")
 
 exports.resetPasswordToken = async (req, res) => {
 	try {
@@ -25,7 +26,8 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		// const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studybyte.vercel.app/update-password/${token}`;
 
 		await mailSender(
 			email,
