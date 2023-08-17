d50def6b1d86ebd4053c250e9a6e6bacb385b3f0
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2023-08-17 17:44:12 +0530

    add responsiveness inside dashboad sidebar

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -7,6 +7,8 @@ import { sidebarLinks } from "../../../data/dashboard-links"
 import { logout } from "../../../services/operations/authAPI"
 import ConfirmationModal from "../../common/ConfirmationModal"
 import SidebarLink from "./SidebarLink"
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+
 
 export default function Sidebar() {
   const { user, loading: profileLoading } = useSelector(
@@ -25,10 +27,27 @@ export default function Sidebar() {
       </div>
     )
   }
+  const ShowOrHideSideBar=()=>{
+    let navigateIconLeft=document.getElementById("NavigateLeftBtn").classList;
+    let navigateIconRight=document.getElementById("NavigateRightBtn").classList;
+    let SideBar=document.getElementById("dashboardSidebar").style;
+    // navigateIconLeft.replace("visible","hidden")
+    if(navigateIconLeft=="visible"){
+      navigateIconLeft.replace("visible","hidden");
+      navigateIconRight.replace("hidden","visible");
+      SideBar.width="50px";
+    }
+    else{
+      navigateIconLeft.replace("hidden","visible");
+      navigateIconRight.replace("visible","hidden");
+      SideBar.width="220px";
+    }
+    
+  }
 
   return (
     <>
-      <div className="flex overflow-auto min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10">
+      <div className="flex overflow-auto  flex-col border-r-[4px] border-r-richblack-700 bg-richblack-800 py-10 relative overflow-x-hidden transition-all" id="dashboardSidebar" >
         <div className="flex flex-col">
           {sidebarLinks.map((link) => {
             if (link.type && user?.accountType !== link.type) return null
@@ -37,7 +56,7 @@ export default function Sidebar() {
             )
           })}
         </div>
-        <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700" />
+        <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700" id="SideBar"/>
         <div className="flex flex-col">
           <SidebarLink
             link={{ name: "Settings", path: "/dashboard/settings" }}
@@ -62,6 +81,10 @@ export default function Sidebar() {
             </div>
           </button>
         </div>
+        <button className="text-white text-4xl absolute bottom-2 right-2"  onClick={ShowOrHideSideBar}>
+            <FaAngleDoubleLeft id="NavigateLeftBtn" className="visible" />
+            <FaAngleDoubleRight id="NavigateRightBtn" className="hidden" />
+          </button>
       </div>
       {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
     </>
--- src/components/core/ViewCourses/VideoDetailsSidebar.jsx
+++ src/components/core/ViewCourses/VideoDetailsSidebar.jsx
@@ -1,60 +1,79 @@
-import { useEffect, useState } from "react"
-import { BsChevronDown } from "react-icons/bs"
-import { IoIosArrowBack } from "react-icons/io"
-import { useSelector } from "react-redux"
-import { useLocation, useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { IoIosArrowBack } from "react-icons/io";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-import IconBtn from "../../common/IconBtn"
+import IconBtn from "../../common/IconBtn";
 
 export default function VideoDetailsSidebar({ setReviewModal }) {
-  const [activeStatus, setActiveStatus] = useState("")
-  const [videoBarActive, setVideoBarActive] = useState("")
-  const navigate = useNavigate()
-  const { sectionId, subSectionId } = useParams()
-  const location = useLocation()
+  const [activeStatus, setActiveStatus] = useState("");
+  const [videoBarActive, setVideoBarActive] = useState("");
+  const navigate = useNavigate();
+  const { sectionId, subSectionId } = useParams();
+  const location = useLocation();
   const {
     courseSectionData,
     courseEntireData,
     totalNoOfLectures,
     completedLectures,
-  } = useSelector((state) => state.viewCourse)
+  } = useSelector((state) => state.viewCourse);
 
   useEffect(() => {
-    ;(() => {
-      if (!courseSectionData.length) return
+    (() => {
+      if (!courseSectionData.length) return;
       const currentSectionIndx = courseSectionData.findIndex(
         (data) => data._id === sectionId
-      )
+      );
       const currentSubSectionIndx = courseSectionData?.[
         currentSectionIndx
-      ]?.SubSection.findIndex((data) => data._id === subSectionId)
-      console.log("couttent subsection index",currentSectionIndx)
-      console.log("this is scourse sectio data",courseSectionData)
+      ]?.SubSection.findIndex((data) => data._id === subSectionId);
+      console.log("couttent subsection index", currentSectionIndx);
+      console.log("this is scourse sectio data", courseSectionData);
       const activeSubSectionId =
         courseSectionData[currentSectionIndx]?.SubSection?.[
           currentSubSectionIndx
         ]?._id;
-        console.log("acitve al".activeSubSectionId);
-        // set current section here
-      setActiveStatus(courseSectionData?.[currentSectionIndx]?._id)
+      console.log("acitve al".activeSubSectionId);
+      // set current section here
+      setActiveStatus(courseSectionData?.[currentSectionIndx]?._id);
       // set current suvsection here
-      setVideoBarActive(activeSubSectionId)
-      
-    })()
+      setVideoBarActive(activeSubSectionId);
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [courseSectionData, courseEntireData, location.pathname])
+  }, [courseSectionData, courseEntireData, location.pathname]);
+
+  const ShowOrHideSideBar=()=>{
+    let navigateIconLeft=document.getElementById("NavigateLeftBtn").classList;
+    console.log(navigateIconLeft)
+    let navigateIconRight=document.getElementById("NavigateRightBtn").classList;
+    let SideBar=document.getElementById("SideBar").style;
+    // navigateIconLeft.replace("visible","hidden")
+    if(navigateIconLeft=="visible"){
+      navigateIconLeft.replace("visible","hidden");
+      navigateIconRight.replace("hidden","visible");
+      SideBar.width="40px";
+    }
+    else{
+      navigateIconLeft.replace("hidden","visible");
+      navigateIconRight.replace("visible","hidden");
+      SideBar.width="320px";
+    }
+    
+  }
 
   return (
     <>
-      <div className="flex h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800">
+      <div className="flex h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 relative transition-all" id="SideBar">
         <div className="mx-5 flex flex-col items-start justify-between gap-2 gap-y-4 border-b border-richblack-600 py-5 text-lg font-bold text-richblack-25">
           <div className="flex w-full items-center justify-between ">
             <div
               onClick={() => {
-                navigate(`/dashboard/enrolled-courses`)
+                navigate(`/dashboard/enrolled-courses`);
               }}
               className="flex h-[35px] w-[35px] items-center justify-center rounded-full bg-richblack-100 p-1 text-richblack-700 hover:scale-90"
-              title="back" 
+              title="back"
             >
               <IoIosArrowBack size={30} />
             </div>
@@ -72,7 +91,7 @@ export default function VideoDetailsSidebar({ setReviewModal }) {
           </div>
         </div>
 
-        <div className="h-[calc(100vh - 5rem)] overflow-y-auto">
+        <div className="h-[calc(100vh - 5rem)] overflow-y-auto ">
           {courseSectionData.map((course, index) => (
             <div
               className="mt-2 cursor-pointer text-sm text-richblack-5"
@@ -114,8 +133,8 @@ export default function VideoDetailsSidebar({ setReviewModal }) {
                       onClick={() => {
                         navigate(
                           `/view-course/${courseEntireData?._id}/section/${course?._id}/sub-section/${topic?._id}`
-                        )
-                        setVideoBarActive(topic._id)
+                        );
+                        setVideoBarActive(topic._id);
                       }}
                     >
                       <input
@@ -130,8 +149,12 @@ export default function VideoDetailsSidebar({ setReviewModal }) {
               )}
             </div>
           ))}
+          <button className="text-white text-4xl absolute bottom-2 right-2"  onClick={ShowOrHideSideBar}>
+            <FaAngleDoubleLeft id="NavigateLeftBtn" className="visible" />
+            <FaAngleDoubleRight id="NavigateRightBtn" className="hidden" />
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
